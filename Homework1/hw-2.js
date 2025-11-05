e3402a08a85b82479ede7fdd994485ca3f10fbc0
Tatiana <tanyabuntar@mail.ru>
2025-11-05 21:51:07 +0500

    создан hw-2.js и внесено 1 задание по  паролю

---
+++ Homework1/hw-2.js
@@ -0,0 +1,13 @@
+// Задание 1
+let password = `пароль`;
+let what = prompt(`Введите пароль`);
+if (what === password) {
+    console.log(`Пароль введен верно`);
+}
+else {
+    console.log(`Пароль введен неправильно`);
+};
+
+// Задание 2
+
+
