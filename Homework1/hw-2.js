9718df6650a002e9eadb57f2aab2d8dd13ae86bb
Tatiana <tanyabuntar@mail.ru>
2025-11-05 22:42:56 +0500

    задание 5 в hw-2

--- Homework1/hw-2.js
+++ Homework1/hw-2.js
@@ -21,3 +21,31 @@ let e = 5;
 let a = '2';
 let b = '3';
 alert(Number(a) + Number(b));
+
+// Задание 5
+let monthNumber = prompt(`Введите номер месяца (1-12)`);
+switch (monthNumber) {
+    case '1':
+    case `2`:
+    case `12`:
+        console.log(`это зима`)
+        break;
+    case `3`:
+    case `4`:
+    case `5`:
+        console.log(`это весна`)
+        break;
+    case `6`:
+    case `7`:
+    case `8`:
+        console.log(`это лето`)
+        break;
+    case `9`:
+    case `10`:
+    case `11`:
+        console.log(`это осень`)
+        break;
+    default:
+        console.log(`Такого месяца не существует`)
+        break;
+}
