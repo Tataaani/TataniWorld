ba9a0dc1112fde16ce5a571db57c3fedb4e418b1
Tatiana <tanyabuntar@mail.ru>
2026-03-15 00:38:21 +0500

    Callback, setTimeout, setInterval. Задание 1-2

--- Homework1/hw-7.js
+++ Homework1/hw-7.js
@@ -39,3 +39,62 @@ let rockPaperScissors = () => {
 
     alert("Спасибо за игру!");
 };
+
+
+// Задание 1
+const people = [
+    { name: 'Глеб', age: 29 },
+    { name: 'Анна', age: 17 },
+    { name: 'Олег', age: 7 },
+    { name: 'Оксана', age: 47 }
+];
+
+people.sort((a, b) => {
+    return a.age - b.age;
+});
+
+console.log(people);
+
+
+// Задание 2
+
+function isPositive(number) {
+    return number > 1;
+}
+
+function isMale(person) {
+    return person.gender === 'female';
+}
+
+function filter(array, ruleFunction) {
+    const result = []; 
+
+    for (let i = 0; i < array.length; i++) {
+        if (ruleFunction(array[i])) {
+            result.push(array[i]);
+        }
+    }
+
+    return result; 
+}
+
+console.log(filter([3, -4, 1, 9], isPositive));
+
+const peoples = [
+   {name: 'Глеб', gender: 'male'},
+   {name: 'Анна', gender: 'female'},
+   {name: 'Олег', gender: 'male'},
+   {name: 'Оксана', gender: 'female'}
+];
+
+console.log(filter(people, isMale)); 
+
+
+// Задание 3
+
+// Задание 4
+
+// Задание 5
+
+
+
