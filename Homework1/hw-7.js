0e4590a1e66942ef776c14205aaeea6ef8d7fe02
Tatiana <tanyabuntar@mail.ru>
2026-03-18 21:58:43 +0500

    Callback, setTimeout, setInterval. Игра и 5 заданий

--- Homework1/hw-7.js
+++ Homework1/hw-7.js
@@ -48,11 +48,9 @@ const people = [
     { name: 'Олег', age: 7 },
     { name: 'Оксана', age: 47 }
 ];
-
 people.sort((a, b) => {
     return a.age - b.age;
 });
-
 console.log(people);
 
 
@@ -61,40 +59,64 @@ console.log(people);
 function isPositive(number) {
     return number > 1;
 }
-
 function isMale(person) {
     return person.gender === 'female';
 }
-
 function filter(array, ruleFunction) {
-    const result = []; 
-
+    const result = [];
     for (let i = 0; i < array.length; i++) {
         if (ruleFunction(array[i])) {
             result.push(array[i]);
         }
     }
-
-    return result; 
+    return result;
 }
-
 console.log(filter([3, -4, 1, 9], isPositive));
-
 const peoples = [
-   {name: 'Глеб', gender: 'male'},
-   {name: 'Анна', gender: 'female'},
-   {name: 'Олег', gender: 'male'},
-   {name: 'Оксана', gender: 'female'}
+    { name: 'Глеб', gender: 'male' },
+    { name: 'Анна', gender: 'female' },
+    { name: 'Олег', gender: 'male' },
+    { name: 'Оксана', gender: 'female' }
 ];
-
-console.log(filter(people, isMale)); 
+console.log(filter(people, isMale));
 
 
 // Задание 3
 
+const timer = (deadline) => {
+    const interval = setInterval(() => {
+        const currentDate = new Date();
+        console.log(currentDate.toLocaleString());
+    }, 3000);
+    setTimeout(() => {
+        clearInterval(interval);
+        console.log(`30 секунд прошло`);
+    }, deadline)
+};
+timer(30000);
+
+
 // Задание 4
+
+function delayForSecond(callback) {
+    setTimeout(() => {
+        callback();
+    }, 1000)
+}
+delayForSecond(function () {
+    console.log('Привет, Глеб!');
+})
+
 
 // Задание 5
 
-
-
+function delayForSecond(cb) {
+    setTimeout(() => {
+        console.log('Прошла одна секунда');
+        if (cb) { cb(); }
+    }, 1000)
+}
+function sayHi(name) {
+    console.log(`Привет, ${name}!`);
+}
+delayForSecond(() => sayHi('Глеб'));
